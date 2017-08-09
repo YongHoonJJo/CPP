ac660db949130e9b0c5870ccfcf5ba73f3890490
ychooni <starctak@gmail.com>
2017-08-09 13:54:41 +0900

    Declaring variables, Typecasing / (kks227.blog.me)

--- Lang/variable.cpp
+++ Lang/variable.cpp
@@ -1,22 +1,21 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int main()
 {
 	int a=5;
-	cout << "a = " << a << endl;
+	printf("a = %d\n", a);
 
 	int b(3), c(4); // Initializing a variable 
-	cout << "b = " << b << endl;
-	cout << "c = " << c << endl;
+	printf("b = %d\n", b);
+	printf("c = %d\n", c);
 
 	for(int i=0; i<5; i++)
-		cout << i << ' ';
-	cout << endl;
+		printf("%d ", i);
+	puts("");
 
 	// TypeCasting
 	double f = 5.5;
 	int n = static_cast<int>(f); // int n = (int)f;
-	cout << f << ' ' << n << endl;
+	printf("%lf %d\n", f, n);
 	
 }
